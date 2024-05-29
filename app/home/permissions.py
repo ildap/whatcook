d7dc8b54316193812e1125bfcc6667d84ca012df
ildar <i.sattarow@ya.ru>
2024-05-29 21:00:19 +0500

    fix permissions

--- app/home/permissions.py
+++ app/home/permissions.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.db import transaction
+
 from rest_framework.permissions import DjangoObjectPermissions
 
 from guardian.shortcuts import assign_perm
@@ -10,12 +13,13 @@ class DjangoObjectPermissionsOrAnonReadOnly(DjangoObjectPermissions):
 class PermissionsMixin:
     permission_classes = [DjangoObjectPermissionsOrAnonReadOnly]
 
+    @transaction.atomic
     def perform_create(self, serializer):
         obj = serializer.save()
         self._assign_object_perms(obj)
 
     def _assign_object_perms(self, object):
-        user = self.request.user,
+        user = self.request.user
         name = object.__class__.__name__.lower()
         assign_perm('home.change_' + name, user, object)
         assign_perm('home.delete_' + name, user, object)
